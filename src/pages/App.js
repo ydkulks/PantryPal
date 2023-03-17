0fcbb8aa642790a340fb8f49de4a6d054aefa52c
ydkulks <ydkulks2@gmail.com>
2023-03-17 11:50:33 +0530

    Getting started

---
+++ src/pages/App.js
@@ -0,0 +1,22 @@
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+// CSS
+import './style/App.css';
+// Pages
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col">
+          <p>Hello world</p>
+        </div>
+        <div className="col">
+          <img src="../assets/bowl_herosection.png" alt="Hero"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
