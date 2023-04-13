46d6a5c024f79cc92b3bc3a77e5e56ed2f7072b3
ydkulks <ydkulks2@gmail.com>
2023-04-13 13:30:10 +0530

    Added another Carousel of Cuisine in Recipe page

--- src/pages/Protected/Recipes.js
+++ src/pages/Protected/Recipes.js
@@ -7,6 +7,14 @@ import img3 from '../../assets/burger.jpg';
 import img4 from '../../assets/cake.jpg';
 import img5 from '../../assets/noodles.jpg';
 import img6 from '../../assets/ice_cream.jpg';
+import usa_cuisine from '../../assets/USA-Cuisine.jpg';
+import british_cuisine from '../../assets/British-Cuisine.jpg';
+import chinese_cuisine from '../../assets/Chinese-Cuisine.jpg';
+import french_cuisine from '../../assets/French-Cuisine.jpg';
+import indian_cuisine from '../../assets/Indian-Cuisine.jpg';
+import italy_cuisine from '../../assets/Italian-Cuisine.jpg';
+import japan_cuisine from '../../assets/Japanese-Cuisine.jpg';
+import mexico_cuisine from '../../assets/Mexican-Cuisine.jpg';
 import React, {useState} from 'react';
 
 const Recipes = () => {
@@ -117,7 +125,7 @@ const Recipes = () => {
                 results by cuisine, dietary restrictions, and cooking time to
                 find the perfect recipe for any occasion.
               </p>
-              <a id="RecipesDownBtn" href="#RecipesSearchBar">
+              <a id="RecipesDownBtn" href="#RecipesSearchContainer">
                 <i className="bi bi-caret-down-fill"></i>
               </a>
             </div>
@@ -125,75 +133,172 @@ const Recipes = () => {
         </div>
       </div>
       <div id="CarouselContainer">
-        <h3 className="CarouselHead">Inspiration for your search</h3>
-        <Carousel id="Carousel" variant="dark">
-          <Carousel.Item className="CarouselItem">
-            <img
-              style={{height: '300px', display: 'block'}}
-              className="d-block w-100"
-              alt="Pizza"
-              src={img1}
-            />
-            <Carousel.Caption>
-              <h3 className="CarouselName">Pizza</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="CarouselItem">
-            <img
-              style={{height: '300px', display: 'block'}}
-              className="d-block w-100"
-              alt="Biryani"
-              src={img2}
-            />
-            <Carousel.Caption>
-              <h3 className="CarouselName">Biryani</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="CarouselItem">
-            <img
-              style={{height: '300px', display: 'block'}}
-              className="d-block w-100"
-              alt="Burger"
-              src={img3}
-            />
-            <Carousel.Caption>
-              <h3 className="CarouselName">Burger</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="CarouselItem">
-            <img
-              style={{height: '300px', display: 'block'}}
-              className="d-block w-100"
-              alt="Cake"
-              src={img4}
-            />
-            <Carousel.Caption>
-              <h3 className="CarouselName">Cake</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="CarouselItem">
-            <img
-              style={{height: '300px', display: 'block'}}
-              className="d-block w-100"
-              alt="Noodles"
-              src={img5}
-            />
-            <Carousel.Caption>
-              <h3 className="CarouselName">Noodles</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="CarouselItem">
-            <img
-              style={{height: '300px', display: 'block'}}
-              className="d-block w-100"
-              alt="Ice cream"
-              src={img6}
-            />
-            <Carousel.Caption>
-              <h3 className="CarouselName">Ice cream</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
+        <div className="row">
+          <div className="col-lg">
+            <h3 className="CarouselHead">Inspiration for your search</h3>
+            <Carousel id="Carousel" indicators={false} variant="dark">
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Pizza"
+                  src={img1}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Pizza</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Biryani"
+                  src={img2}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Biryani</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Burger"
+                  src={img3}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Burger</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Cake"
+                  src={img4}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Cake</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Noodles"
+                  src={img5}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Noodles</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Ice cream"
+                  src={img6}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Ice cream</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+          <div className="col-lg">
+            <h3 className="CarouselHead">Cuisine</h3>
+            <Carousel id="Carousel" indicators={false} variant="dark">
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="American"
+                  src={usa_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">American</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="British"
+                  src={british_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">British</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Chinese"
+                  src={chinese_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Chinese</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="French"
+                  src={french_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">French</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Indian"
+                  src={indian_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Indian</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Italian"
+                  src={italy_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Italian</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Japanese"
+                  src={japan_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Japanese</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className="CarouselItem">
+                <img
+                  style={{height: '300px', display: 'block'}}
+                  className="d-block w-100"
+                  alt="Mexican"
+                  src={mexico_cuisine}
+                />
+                <Carousel.Caption>
+                  <h3 className="CarouselName">Mexican</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </div>
       </div>
       <div id="RecipesSearchContainer">
         <div className="form-row">
@@ -220,7 +325,6 @@ const Recipes = () => {
                   <option value="DEFAULT" disabled>
                     Diet
                   </option>
-                  <option value="GlutenFree">Gluten Free</option>
                   <option value="Ketogenic">Ketogenic</option>
                   <option value="Vegetarian">Vegetarian</option>
                   <option value="Lacto-Vegetarian">Lacto-Vegetarian</option>
@@ -229,7 +333,6 @@ const Recipes = () => {
                   <option value="Pescetarian">Pescetarian</option>
                   <option value="Paleo">Paleo</option>
                   <option value="Primal">Primal</option>
-                  <option value="LowFODMAP">LowFODMAP</option>
                   <option value="Whole30">Whole30</option>
                 </select>
                 <select
@@ -248,7 +351,6 @@ const Recipes = () => {
                   <option value="Italian">Italian</option>
                   <option value="Japanese">Japanese</option>
                   <option value="Mexican">Mexican</option>
-                  <option value="MiddleEastern">MiddleEastern</option>
                 </select>
                 <br />
                 <label htmlFor="ingInput">Ingredients</label>
