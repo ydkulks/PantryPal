f0d4e8a82f01f80348791c4f40fb0fc0fde082cf
ydkulks <ydkulks2@gmail.com>
2023-07-09 19:42:32 +0530

    Added shopping list endpoints for adding and getting list; and print list dynamically(no stying)

--- server.mjs
+++ server.mjs
@@ -1,43 +1,51 @@
 /* add "type":"module" in package.json (require)
  * or rename server.js to server.mjs (import)
  */
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 5000;
-import fetch from 'node-fetch'; //Fetch
-import Datastore from 'nedb'; //DB
+import fetch from "node-fetch"; //Fetch
+import Datastore from "nedb"; //DB
 const cf = new Datastore({
-  filename: './database/contactForm.db',
+  filename: "./database/contactForm.db",
   autoload: true,
 });
-const su = new Datastore({filename: './database/signUp.db', autoload: true});
-import dotenv from 'dotenv'; //Env
+const su = new Datastore({
+  filename: "./database/signUp.db",
+  autoload: true,
+});
+const sl = new Datastore({
+  filename: "./database/shoppinglist.db",
+  autoload: true,
+});
+import dotenv from "dotenv"; //Env
 dotenv.config();
-import cors from 'cors'; //CORS
+import cors from "cors"; //CORS
 app.use(cors());
 app.use(express.json());
-import bcrypt from 'bcrypt'; //Encrypt
-import jwt from 'jsonwebtoken'; //Auth
+import bcrypt from "bcrypt"; //Encrypt
+import jwt from "jsonwebtoken"; //Auth
 
 /******* End-point index *******/
 // Contact form
 // Sign-up
 // Login
-// Authorisation
+// Authorization
 // Protected route
 // Account deletion
 // Dashboard
 // Recipes API
 // Instructions API
-app.post('/api/contact', (req, res) => {
+// Shopping List
+app.post("/api/contact", (req, res) => {
   //console.log(req.body);
   cf.insert(req.body);
-  res.send({status: 200, contactForm: 'Submitted'});
+  res.send({ status: 200, contactForm: "Submitted" });
 });
 
-app.post('/api/signup', async (req, res) => {
+app.post("/api/signup", async (req, res) => {
   const hashedPwd = await bcrypt.hash(req.body.password, 10);
-  su.find({name: req.body.name, email: req.body.email}, (err, doc) => {
+  su.find({ name: req.body.name, email: req.body.email }, (err, doc) => {
     // if not found, doc's value is []
     if (doc.length === 0) {
       const record = {
@@ -47,47 +55,48 @@ app.post('/api/signup', async (req, res) => {
         role: basic,
       };
       su.insert(record, (err, newDoc) => {});
-      res.send({status: 201, signup: 'Created'});
+      res.send({ status: 201, signup: "Created" });
     } else {
-      res.send({status: 409, signup: 'Conflict'});
+      res.send({ status: 409, signup: "Conflict" });
     }
   });
 });
 
 const ENV = process.env;
-app.post('/api/login', (req, res) => {
+app.post("/api/login", (req, res) => {
   // Authentication
-  su.find({name: req.body.name}, async (err, doc) => {
+  su.find({ name: req.body.name }, async (err, doc) => {
     if (doc.length === 0) {
-      res.send({status: 404, login: 'Not Found'});
+      res.send({ status: 404, login: "Not Found" });
     } else {
       const Decrypt = await bcrypt.compare(req.body.password, doc[0].password);
 
       if (Decrypt === true) {
-        // Authorisation
+        // Authorization
         // Assigning token to user
         const accessToken = jwt.sign(doc[0].name, ENV.ACCESS_TOKEN_SECRET);
         res.send({
           status: 200,
-          login: 'Ok',
+          login: "Ok",
           token: accessToken,
         });
       } else {
-        res.send({status: 401, login: 'Unauthorized'});
+        res.send({ status: 401, login: "Unauthorized" });
       }
     }
   });
 });
 
-const Authorisation = (req, res, next) => {
-  const authHeader = req.headers['authorisation'];
-  const token = authHeader && authHeader.split(' ')[1];
+const Authorization = (req, res, next) => {
+  const authHeader = req.headers["authorisation"];
+  const token = authHeader && authHeader.split(" ")[1];
   if (token === null) {
-    return res.send({status: 401});
+    return res.send({ status: 401 });
   } else {
     jwt.verify(token, ENV.ACCESS_TOKEN_SECRET, (err, name) => {
       if (err) {
-        return res.send({status: 401, error: 'Unauthorized'});
+        console.log(err);
+        return res.send({ status: 401, error: "Unauthorized" });
       } else {
         req.user = name;
         next();
@@ -96,44 +105,44 @@ const Authorisation = (req, res, next) => {
   }
 };
 
-app.get('/api/protected', Authorisation, (req, res) => {
+app.get("/api/protected", Authorization, (req, res) => {
   // Checking role of the user
-  su.find({name: req.user}, (err, doc) => {
+  su.find({ name: req.user }, (err, doc) => {
     if (err) console.log(err);
-    if (doc.length === 0) return res.send({status: 401});
-    if (doc[0].role === 'admin') {
-      return res.send({status: 200, role: 'admin'});
+    if (doc.length === 0) return res.send({ status: 401 });
+    if (doc[0].role === "admin") {
+      return res.send({ status: 200, role: "admin" });
     }
-    if (doc[0].role === 'basic') {
-      return res.send({status: 200, role: 'basic'});
+    if (doc[0].role === "basic") {
+      return res.send({ status: 200, role: "basic" });
     }
   });
 });
 
-app.get('/api/deleteAccount', Authorisation, (req, res) => {
-  su.remove({name: req.user});
-  return res.send({status: 200, message: 'account deleted'});
+app.get("/api/deleteAccount", Authorization, (req, res) => {
+  su.remove({ name: req.user });
+  return res.send({ status: 200, message: "account deleted" });
 });
 
-app.get('/api/dashboard', Authorisation, (req, res) => {
-  su.find({name: req.user}, (err, doc) => {
+app.get("/api/dashboard", Authorization, (req, res) => {
+  su.find({ name: req.user }, (err, doc) => {
     if (err) console.log(err);
     if (doc.length === 0) return res.status(401);
-    if (doc[0].role === 'admin') {
+    if (doc[0].role === "admin") {
       return res.status(200).send({
         name: doc[0].name,
         role: doc[0].role,
       });
     }
-    if (doc[0].role === 'basic') {
-      return res.status(200).send({name: doc[0].name});
+    if (doc[0].role === "basic") {
+      return res.status(200).send({ name: doc[0].name });
     }
   });
 });
 
 // Calling 'spoonacular' API
 const apiKey = process.env.API_KEY;
-app.post('/api/recipes', async (req, res) => {
+app.post("/api/recipes", async (req, res) => {
   const query = req.body.query;
   const diet = req.body.diet;
   const cuisine = req.body.cuisine;
@@ -148,13 +157,40 @@ app.post('/api/recipes', async (req, res) => {
   res.status(200).send(data);
 });
 
-app.post('/api/instructions', async (req, res) => {
+app.post("/api/instructions", async (req, res) => {
   const id = req.body.id;
   const params = `${id}/analyzedInstructions?apiKey=${apiKey}`;
   const url = `https://api.spoonacular.com/recipes/${params}`;
   const request = await fetch(url);
   const data = await request.json();
   res.status(200).send(data);
+});
+
+app.post("/api/shoppinglist", Authorization, (req, res) => {
+  const NewData = {
+    name: req.user,
+    item: req.body.item,
+    timestamp: req.body.timestamp,
+  };
+  // Insert new data into the database
+  sl.insert(NewData);
+  // Send updated data to client
+  sl.find({ name: req.user }, (err, doc) => {
+    if (err) return console.log(err);
+    if (doc.length === 0) return res.send({ status: 401 });
+
+    res.send({ status: 200, data: doc });
+  });
+});
+
+app.get("/api/shoppinglist", Authorization, (req, res) => {
+  // Send user's list to the client
+  sl.find({ name: req.user }, (err, doc) => {
+    if (err) return console.log(err);
+    if (doc.length === 0) return res.send({ status: 401 });
+
+    res.send({ status: 200, data: doc });
+  });
 });
 
 app.listen(port, () => {
--- src/pages/Protected/ShoppingList.js
+++ src/pages/Protected/ShoppingList.js
@@ -1,8 +1,94 @@
 import AuthUser from '../AuthUser';
+import React, {useState, useEffect} from 'react';
+
 const ShoppingList = () => {
+  useEffect(() => {
+    const getList = async () => {
+      const token = localStorage.getItem('token');
+      try {
+        const request = await fetch('http://localhost:5000/api/shoppinglist', {
+          headers: {
+            Authorisation: `Bearer ${token}`,
+          },
+        });
+        const response = await request.json();
+        if (response.status === 200) {
+          const items = [];
+          // Extracting items from object and storing in array
+          response.data.forEach(item => {
+            items.push(item.item);
+          });
+          items.sort((a, b) => a.localeCompare(b));
+          setResultData(items);
+          //console.log('All Data: ', response);
+        } else {
+          console.log('Server Error: ', response);
+        }
+      } catch (err) {
+        console.log('Error:', err);
+      }
+    };
+    getList();
+  }, []);
+
+  const [item, setItem] = useState(null);
+  const [resultData, setResultData] = useState([]);
+  const token = localStorage.getItem('token');
+
+  const SubmitForm = async event => {
+    const formData = {
+      item: item,
+      timestamp: Date(),
+    };
+    event.preventDefault();
+    event.target.reset();
+
+    try {
+      const request = await fetch('http://localhost:5000/api/shoppinglist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorisation: `Bearer ${token}`,
+        },
+        body: JSON.stringify(formData),
+      });
+      const response = await request.json();
+      if (response.status === 200) {
+        const items = [];
+        // Extracting items from object and storing in array
+        response.data.forEach(item => {
+          items.push(item.item);
+        });
+        // sorting array
+        items.sort((a, b) => a.localeCompare(b));
+        setResultData(items);
+      } else {
+        console.log('Server Error!');
+      }
+    } catch (err) {
+      console.log('Error:', err);
+    }
+  };
   return (
     <div className="SListContainer">
-      <h2 className="SListTitle">Shopping List</h2>
+      <div id="InstHero" className="SListHero">
+        <h2 className="SListTitle">Shopping List</h2>
+        <div>
+          <form id="SListForm" onSubmit={SubmitForm}>
+            <input
+              onChange={e => setItem(e.target.value)}
+              type="text"
+              placeholder="Enter shopping item..."
+            />
+            <button type="submit">Add</button>
+          </form>
+        </div>
+      </div>
+      <div>
+        {resultData.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
     </div>
   );
 };
